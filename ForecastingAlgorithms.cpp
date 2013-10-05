462df1b001fb1cd7e24d082b04038b9003cd3332
Konstantin <Empty>
2013-10-05 21:03:56 +0700

    Two algorithms were implemented

--- ForecastingAlgorithms.cpp
+++ ForecastingAlgorithms.cpp
@@ -3,18 +3,81 @@
 using namespace std;
 using namespace ForecastingAlgorithms;
 
-MovingAverage::MovingAverage(int n)
+//-----------------------------------------------------------------
+// Moving Average Implementation
+//-----------------------------------------------------------------
+
+MovingAverage::MovingAverage(const int n) : base(n)
 {
-    cout << n << endl;
+
 }
 
-Prediction MovingAverage::DoPrediction(Value value, Prediction history)
+//-----------------------------------------------------------------
+
+Value MovingAverage::DoPrediction(Value value, History history)
 {
-    cout << "Yes!" << endl;
+    Value result = 0;
+    int t = history.size();
+    int base = GetBase();
+    if( t < base )
+    {
+        throw IncorrectHistoryException("base bigger then history size.");
+    }
+
+    for(int i = 0; i < base; i++)
+    {
+        result += history[t - base + i];
+    }
+
+    result /= base;
+
+    return result;
+}
+
+//-----------------------------------------------------------------
+// Exponential Smoothing Implementation
+//-----------------------------------------------------------------
+
+ExponentialSmoothing::ExponentialSmoothing(const double a) : base(a)
+{
+
+}
+
+//-----------------------------------------------------------------
+
+Value ExponentialSmoothing::DoPrediction(Value value, History history)
+{
+    int t = history.size()-1;
+    double a = GetBase();
+    Value result = a*value;
+
+    while(t != 0)
+    {
+        result += a*pow(1-a, history.size()-t)*history[t];
+        t--;
+    }
+
+    return result;
 }
 
 int main()
-{
-    MovingAverage mv(5);
+try{
+    MovingAverage mv(4);
+    ExponentialSmoothing es(0.3);
+    History p(4);
+    p[0] = 2;
+    p[1] = 3;
+    p[2] = 2;
+    p[3] = 5;
+
+    cout << mv.DoPrediction(4, p) << endl;
+    cout << es.DoPrediction(4, p) << endl;
+
     return 0;
+}
+
+catch(IncorrectHistoryException e)
+{
+    std::cerr << "Error: " << e.GetMessage() << endl;
+    return 1;
 }
--- ForecastingAlgorithms.h
+++ ForecastingAlgorithms.h
@@ -3,26 +3,58 @@
 
 #include <vector>
 #include <iostream>
+#include <cmath>
 
 namespace ForecastingAlgorithms
 {
-    typedef std::vector<double> Prediction;
+    typedef std::vector<double> History;
     typedef double Value;
+
+    //-----------------------------------------------------------------
 
     class ForecastingAlgorithm
     {
         public:
-            virtual Prediction DoPrediction(Value value, Prediction history) = 0;
+            virtual Value DoPrediction(Value value, History history) = 0;
             virtual ~ForecastingAlgorithm() {};
         protected:
             ForecastingAlgorithm() {};
     };
 
+    //-----------------------------------------------------------------
+
     class MovingAverage : public ForecastingAlgorithm
     {
         public:
             MovingAverage(int n);
-            Prediction DoPrediction(Value value, Prediction history);
+            Value DoPrediction(Value value, History history);
+
+            inline int GetBase() { return base; }
+        private:
+            int base;
+    };
+
+    //-----------------------------------------------------------------
+
+    class ExponentialSmoothing : public ForecastingAlgorithm
+    {
+        public:
+            ExponentialSmoothing(const double a);
+            Value DoPrediction(Value value, History history);
+            double GetBase() { return base; }
+        private:
+            double base;
+    };
+
+    //-----------------------------------------------------------------
+
+    class IncorrectHistoryException
+    {
+        public:
+            IncorrectHistoryException(std::string msg) : message(msg) {};
+            std::string GetMessage() { return message; };
+        private:
+            std::string message;
     };
 
 } // of ForecastingAlgorithms
